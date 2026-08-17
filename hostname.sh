2257781f4f454b2a81c1f760772cf9ce37f0f5d0
SANGAVIS3016 <sangavis@rvsgroup.com>
2026-08-17 14:30:47 +0530

    Update hostname.sh

--- hostname.sh
+++ hostname.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
 
-# Unix Practical: Change the Hostname in Linux
-
-# Student Name:
-# Register Number:
-
-# Step 1: Check the current hostname
-# Write your command below
-
-
-# Step 2: Change the hostname to rvs
-# Write your command below
-
-
-# Step 3: Start a new Bash shell
-# Write your command below
